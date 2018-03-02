b0e3dd2b1f406cad2344699d53563979e1dd3d3a
Priyal Patel <priyal.patel@externals.hilti.com>
2018-03-02 14:52:13 -0600

    Initial commit

---
+++ test/ApplicationTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+  @Test
+  public void simpleCheck() {
+    int a = 1 + 1;
+    assertEquals(2, a);
+  }
+}
---
+++ test/IntegrationTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class IntegrationTest {
+
+  @Test
+  public void test() {
+    assertTrue(true);
+  }
+}
